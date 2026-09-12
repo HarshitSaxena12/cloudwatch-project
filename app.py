692794905ce9646098b4c713c644b5d749611543
Harshit Saxena <hsaxena869@gmail.com>
2026-09-12 20:56:15 +0000

    Add dashboard  table with dynamic status styling

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return  "CloudWatch Dashboard - Coming Soon"
 
-if __name__  == '__main__':
+
+@app.route('/')
+
+def home():
+
+    resources = [
+
+        {"name": "Web-Server-01", "type": "Virtual Machine", "status": "Running"},
+
+        {"name": "Storage-Prod", "type": "Storage Account", "status": "Running"},
+
+        {"name": "DB-Backup-VM", "type": "Virtual Machine", "status": "Stopped"}
+
+    ]
+
+    return render_template('dashboard.html', resources=resources)
+
+
+
+if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
